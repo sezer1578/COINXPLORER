8894d71c877f04596d62f4631b8a9b21f5d6686f
Sezer Özaltun <sezer_ozaltun@hotmail.com>
2023-12-23 20:37:48 +0300

    appicon and some code refactor

--- app/src/main/java/com/ozaltun/coinxplorer/presentation/screens/onboarding/components/OnBoardingPage.kt
+++ app/src/main/java/com/ozaltun/coinxplorer/presentation/screens/onboarding/components/OnBoardingPage.kt
@@ -1,5 +1,4 @@
 package com.ozaltun.coinxplorer.presentation.screens.onboarding.components
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,13 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import com.ozaltun.coinxplorer.R
 import com.ozaltun.coinxplorer.presentation.screens.onboarding.Page
-import com.ozaltun.coinxplorer.presentation.ui.theme.COINXPLORERTheme
 import com.ozaltun.coinxplorer.util.constant.Dimens.MediumPadding1
 import com.ozaltun.coinxplorer.util.constant.Dimens.MediumPadding2
 
@@ -27,6 +25,7 @@ fun OnBoardingPage(
     modifier: Modifier = Modifier,
     page: Page,
 ) {
+    val context = LocalContext.current
     Column(modifier = modifier) {
         Image(
             modifier = Modifier
@@ -39,30 +38,15 @@ fun OnBoardingPage(
         Spacer(modifier = Modifier.height(MediumPadding1))
         Text(
             modifier = Modifier.padding(horizontal = MediumPadding2),
-            text = page.title,
+            text = context.getString(page.title),
             style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
             color = colorResource(id = R.color.display_small)
         )
         Text(
             modifier = Modifier.padding(horizontal = MediumPadding2),
-            text = page.description,
+            text = context.getString(page.description),
             style = MaterialTheme.typography.bodyMedium,
             color = colorResource(id = R.color.text)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun OnBoardingPagePreview() {
-    COINXPLORERTheme {
-        OnBoardingPage(
-            page = Page(
-                title = "Lorem Ipsum is simply dummy",
-                description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                image = R.drawable.onboarding1
-            )
         )
     }
 }
